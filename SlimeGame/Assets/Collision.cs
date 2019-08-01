804f7797b77b16a18f77d9002be7aa5005700cb6
Andrew <smithja@ferris.edu>
2019-08-01 01:47:47 -0400

    You can now eat objects
    - when you move over an object if your weight is more than the objects weight it is consumed and destroyed
    - If it is less than the objects weight it takes damage untill it's weight is 0 then it is destroyed
    -TODO: slow down the eating rate
    -TODO: make the player gain rate for eating the object based on the objects weight and it's classification
    -TODO:  make the slime stop eating if it is not touching the object

--- SlimeGame/Assets/Collision.cs
+++ SlimeGame/Assets/Collision.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    public int weight= 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,26 @@ public class Collision : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("SmallObject")) Eat(other.gameObject);
-    }
-    private void OnTriggerEnter(UnityEngine.Collision collision)
-    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("SmallObject"))
+        {
+            if(other.gameObject.GetComponent<ObjectController>().wieght > weight)
+            {
+                Eat(other.gameObject);
+            }
+            else
+            {
+                Destroy(other.gameObject);
+            }
+        }
         
-
     }
+    
 
     private void Eat(GameObject objectToEat)
     {
+        objectToEat.GetComponent<ObjectController>().BeingEaten(weight);
         print("Eating Object");
     }
 }
---
+++ SlimeGame/Assets/ObjectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public int wieght = 25; //how long the object takes to get eaten
+    public bool consumed = false;//Is the object completely eaten
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void BeingEaten(int damage)
+    {
+        wieght -= damage;
+        print("OH NO My My Current health is " + wieght);
+        if (wieght <= 0) consumed = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

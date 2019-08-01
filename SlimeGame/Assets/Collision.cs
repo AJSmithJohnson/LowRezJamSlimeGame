955fed74d76afeb777aa5ed3dc415235ea98c717
Andrew <smithja@ferris.edu>
2019-08-01 01:27:00 -0400

    We can eat objects
    - Using OnTriggerEnter and LayerMask.NameToLayer to detect when we intersect a small object
    - TODO: delete objects when they are completely digested
    -TODO: compare an objects weight and the slimes weight to determine how fast the object is being eaten

--- SlimeGame/Assets/Collision.cs
+++ SlimeGame/Assets/Collision.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,5 +15,19 @@ public class Collision : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("SmallObject")) Eat(other.gameObject);
+    }
+    private void OnTriggerEnter(UnityEngine.Collision collision)
+    {
+        
+
+    }
+
+    private void Eat(GameObject objectToEat)
+    {
+        print("Eating Object");
     }
 }
